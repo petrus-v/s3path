aa51a583ef62a12547ab1cf79e0ecdfc2dec05e6
Pierre Verkest <pierreverkest84@gmail.com>
2022-11-02 16:53:37 +0100

    FIX #88: Implement rename from/to S3 file

--- README.rst
+++ README.rst
@@ -147,6 +147,21 @@ Or Simply reading:
    </html>
    """
 
+
+Rename file between s3 path or to/from file system
+
+.. code:: python
+
+   >>> s3_path = S3Path('/pypi-proxy/botocore/index.html').rename("/tmp/test.html")
+   >>> local_index_path = s3_path.rename("/tmp/test.html")
+   >>> local_index_path
+   PosixPath("/tmp/test.html")
+   >>> local_index_path.exists()
+   True
+   >>> s3_path.exists()
+   False
+
+
 Requirements:
 =============
 
--- s3path.py
+++ s3path.py
@@ -19,12 +19,16 @@ try:
     import smart_open
     from s3transfer.manager import TransferManager
     ALLOWED_COPY_ARGS = TransferManager.ALLOWED_COPY_ARGS
+    ALLOWED_UPLOAD_ARGS = TransferManager.ALLOWED_UPLOAD_ARGS
+    ALLOWED_DOWNLOAD_ARGS = TransferManager.ALLOWED_DOWNLOAD_ARGS
 except ImportError:
     boto3 = None
     ClientError = Exception
     LazyLoadedDocstring = None
     smart_open = None
     ALLOWED_COPY_ARGS = []
+    ALLOWED_UPLOAD_ARGS = []
+    ALLOWED_DOWNLOAD_ARGS = []
 
 __version__ = '0.3.4'
 __all__ = (
@@ -285,6 +289,27 @@ class _S3Accessor(_Accessor):
                 allowed_extra_args=ALLOWED_COPY_ARGS,
             )
             self._boto3_method_with_parameters(object_summary.delete)
+
+    def put(self, source, target):
+        resource, config = self.configuration_map.get_configuration(target)
+        bucket = resource.Bucket(target.bucket)
+        self._boto3_method_with_extraargs(
+            bucket.upload_file,
+            config=config,
+            args=(str(source), target.key, ),
+            allowed_extra_args=ALLOWED_UPLOAD_ARGS,
+        )
+
+    def get(self, source, target):
+        resource, config = self.configuration_map.get_configuration(source)
+        s3_object = resource.Bucket(source.bucket).Object(source.key)
+        with open(target, 'wb') as data:
+            self._boto3_method_with_extraargs(
+                s3_object.download_fileobj,
+                config=config,
+                args=(data,),
+                allowed_extra_args=ALLOWED_DOWNLOAD_ARGS,
+            )
 
     def replace(self, path, target):
         return self.rename(path, target)
@@ -750,10 +775,30 @@ class S3Path(_PathNotSupportedMixin, Path, PureS3Path):
         Target can be either a string or another S3Path object.
         """
         self._absolute_path_validation()
+        if isinstance(target, str):
+            target = Path(target) 
         if not isinstance(target, type(self)):
-            target = type(self)(target)
+            return self._rename_to_fs(target)
         target._absolute_path_validation()
         return super().rename(target)
+
+    def _rename_to_fs(self, target):
+        self._accessor.get(self, target)
+        if not target.exists():
+            raise Exception(
+                "Something goes wrong while renaming s3 object from %s to %s " % (self, target)
+            )
+        self.unlink()
+        return target
+
+    def _rename_from_fs(self, source):
+        self._accessor.put(source, self)
+        if not self.exists():
+            raise Exception(
+                "Something goes wrong while renaming file from %s to s3 object %s " % (source, self)
+            )
+        source.unlink()
+        return self
 
     def replace(self, target):
         """
@@ -875,7 +920,6 @@ class S3Path(_PathNotSupportedMixin, Path, PureS3Path):
         if template is None:
             self._accessor = _s3_accessor
 
-
 class StatResult(namedtuple('BaseStatResult', 'size, last_modified')):
     """
     Base of os.stat_result but with boto3 s3 features
@@ -939,6 +983,17 @@ def s3_factory_and_sanitize_args(cls, cls_if_s3, *args):
 original_path_new = Path.__new__
 original_purepath_new = PurePath.__new__
 
+original_path_rename = Path.rename
+
+def __mokey_patch_path_rename__(self, target):
+    if target.__class__ is str:
+        target = Path(target)
+    if self.__class__ is S3Path and target.__class__ is not S3Path:
+        return self._rename_to_fs(target)
+    elif self.__class__ is not S3Path and target.__class__ is S3Path:
+        return target._rename_from_fs(self)
+    return original_path_rename(self, target)
+
 def __monkey_patch_path__new__(cls, *args, **kwargs):
     if cls is Path:
         cls, args = s3_factory_and_sanitize_args(cls, S3Path, *args)
@@ -952,3 +1007,5 @@ def __monkey_patch_purepath__new__(cls, *args, **kwargs):
 
 Path.__new__ = __monkey_patch_path__new__
 PurePath.__new__ = __monkey_patch_purepath__new__
+
+Path.rename = __mokey_patch_path_rename__
